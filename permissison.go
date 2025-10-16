2b7068bf01b6443459ff3f3cb045c85b6bbeb478
muchtar-syarief <mukhtar.syariefudin1999@gmail.com>
2025-10-16 13:10:15 +0700

    rep: :closed_lock_with_key: register permission accounting

---
+++ permissison.go
@@ -0,0 +1,111 @@
+package accounting_service
+
+import (
+	"fmt"
+
+	"github.com/pdcgo/accounting_service/accounting_model"
+	"github.com/pdcgo/shared/authorization"
+	"github.com/pdcgo/shared/db_models"
+	"github.com/pdcgo/shared/interfaces/authorization_iface"
+	"gorm.io/gorm"
+)
+
+type RoleItem map[string]authorization_iface.RoleAddPermissionPayload
+
+type RoleMap map[db_models.TeamType]RoleItem
+
+var fullAccess = []*authorization_iface.RoleAddPermissionItem{
+	{
+		Action: authorization_iface.Create,
+		Policy: authorization_iface.Allow,
+	},
+	{
+		Action: authorization_iface.Read,
+		Policy: authorization_iface.Allow,
+	},
+	{
+		Action: authorization_iface.Update,
+		Policy: authorization_iface.Allow,
+	},
+	{
+		Action: authorization_iface.Delete,
+		Policy: authorization_iface.Allow,
+	},
+}
+
+var accountingPermission authorization_iface.RoleAddPermissionPayload = authorization_iface.RoleAddPermissionPayload{
+	&accounting_model.BankTransfer{}:  fullAccess,
+	&accounting_model.ExpenseEntity{}: fullAccess,
+	&accounting_model.Payment{}:       fullAccess,
+}
+
+func defaultRootRolePermission() RoleMap {
+	roleMap := RoleMap{
+		db_models.RootTeamType: {},
+		db_models.AdminTeamType: RoleItem{
+			"owner": accountingPermission,
+			"admin": accountingPermission,
+		},
+		db_models.SellingTeamType:   RoleItem{},
+		db_models.WarehouseTeamType: RoleItem{},
+	}
+
+	return roleMap
+}
+
+func defaultRolePermission() RoleMap {
+	roleMap := RoleMap{
+		db_models.RootTeamType: {},
+		db_models.AdminTeamType: RoleItem{
+			"owner": accountingPermission,
+			"admin": accountingPermission,
+		},
+		db_models.SellingTeamType: RoleItem{
+			"owner": accountingPermission,
+			"admin": accountingPermission,
+		},
+		db_models.WarehouseTeamType: RoleItem{
+			"owner": accountingPermission,
+			"admin": accountingPermission,
+		},
+	}
+
+	return roleMap
+}
+
+func RegisterPermission(db *gorm.DB, teamID uint, teamType db_models.TeamType) error {
+	var err error
+
+	roleMap := defaultRolePermission()
+	rootRoleMap := defaultRootRolePermission()
+
+	roleItem, ok := roleMap[teamType]
+	if !ok {
+		err = fmt.Errorf("team type %s not found in role map", teamType)
+		return err
+	}
+
+	domain := authorization.NewDomainV2(db, teamID)
+	for roleKey, permissions := range roleItem {
+		err := domain.RoleAddPermission(roleKey, permissions)
+		if err != nil {
+			return err
+		}
+	}
+
+	rootRoleItem := rootRoleMap[teamType]
+	if !ok {
+		err = fmt.Errorf("team type %s not found in root role map", teamType)
+		return err
+	}
+
+	rootDomain := authorization.NewDomainV2(db, authorization.RootDomain)
+	for roleKey, permissions := range rootRoleItem {
+		err := rootDomain.RoleAddPermission(roleKey, permissions)
+		if err != nil {
+			return err
+		}
+	}
+
+	return err
+}
